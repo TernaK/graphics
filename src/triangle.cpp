c03ebae0b754ac1d1cfa0f5e686b7c1748602574
Terna <tkpamber@yahoo.com>
2018-02-07 17:04:47 -0800

    add key callback

--- src/triangle.cpp
+++ src/triangle.cpp
@@ -6,11 +6,6 @@
 using namespace std;
 
 namespace helper {
-  void error_callback(int err, const char* desc) {
-    fprintf(stderr, "error [%d]: %s\n", err, desc);
-    glfwTerminate();
-  }
-  
   GLFWwindow* make_window(int w, int h, std::string name) {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -21,6 +16,26 @@ namespace helper {
       throw std::runtime_error("window could not be created");
     return window;
   }
+  
+  void error_callback(int err, const char* desc) {
+    fprintf(stderr, "error [%d]: %s\n", err, desc);
+    glfwTerminate();
+  }
+  
+  void resize_callback(GLFWwindow* window, int w, int h) {
+    glViewport(0, 0, w, h);
+  }
+  
+  void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if(action == GLFW_RELEASE) {
+      switch(key) {
+        case GLFW_KEY_ESCAPE:
+          glfwSetWindowShouldClose(window, GLFW_TRUE);
+          break;
+      }
+    }
+  }
+  
 }
 
 int main(int argc, char* args[]) {
@@ -39,9 +54,16 @@ int main(int argc, char* args[]) {
     exit(EXIT_FAILURE);
   }
   
+  glfwSetFramebufferSizeCallback(window, helper::resize_callback);
+  glfwSetKeyCallback(window, helper::key_callback);
+  
   /* code */
   while(!glfwWindowShouldClose(window)) {
+    glClearColor(0.1, 0.4, 0.2, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    
     glfwPollEvents();
+    glfwSwapBuffers(window);
   }
   /* code */
   
