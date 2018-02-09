7aca4489ce557f996b3af5ecad10ec2caac9a1d8
Terna <tkpamber@yahoo.com>
2018-02-09 14:19:30 -0800

    improve error reporting

--- include/graphics/util.h
+++ include/graphics/util.h
@@ -25,7 +25,22 @@ namespace graphics {
     void check_gl_errors() {
       GLenum err;
       while((err = glGetError()) && err != GL_NO_ERROR) {
-        throw std::runtime_error(std::string("GL_ERROR(" + std::to_string(err) + ")\n"));
+        switch(err) {
+          case GL_INVALID_ENUM:
+            throw std::runtime_error(std::string("GL_ERROR(GL_INVALID_ENUM)\n"));
+            break;
+          case GL_INVALID_OPERATION:
+            throw std::runtime_error(std::string("GL_ERROR(GL_INVALID_OPERATION)\n"));
+            break;
+          case GL_INVALID_VALUE:
+            throw std::runtime_error(std::string("GL_ERROR(GL_INVALID_VALUE)\n"));
+            break;
+          case GL_INVALID_FRAMEBUFFER_OPERATION:
+            throw std::runtime_error(std::string("GL_ERROR(GL_INVALID_FRAMEBUFFER_OPERATION)\n"));
+            break;
+          default:
+            throw std::runtime_error(std::string("GL_ERROR(" + std::to_string(err) + ")\n"));
+        }
       }
     }
 
