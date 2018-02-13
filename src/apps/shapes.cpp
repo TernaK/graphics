6b542925a3dca28c4f75ad8fd05905996abb2024
Terna <tkpamber@yahoo.com>
2018-02-13 13:54:07 -0800

    update application

--- src/apps/shapes.cpp
+++ src/apps/shapes.cpp
@@ -6,13 +6,20 @@ int main(int argc, char* args[]) {
   shared_ptr<graphics::Canvas> canvas = make_shared<graphics::Canvas>();
   graphics::Scene3D scene(canvas);
   
-  std::shared_ptr<graphics::Solid> solid = graphics::SolidShape::make_sphere(3, 10, 10);
+  std::shared_ptr<graphics::Solid> earth = graphics::SolidShape::make_sphere(2, 10, 10);
+  earth->material.color = glm::vec3(0.1, 0.1, 0.8);
+  std::shared_ptr<graphics::Solid> moon = graphics::SolidShape::make_sphere(0.3, 7, 7);
+  moon->material.color = glm::vec3(0.4, 0.4, 0.4);
   
-  scene.add_drawable(solid);
+  scene.add_drawable(earth);
+  scene.add_drawable(moon);
 
   while(!glfwWindowShouldClose(canvas->window)) {
-    solid->rotation.y += 1;
-    solid->rotation.x += 1;
+    earth->rotation.y += 1;
+    earth->rotation.x += 1;
+    moon->rotation.x += 1;
+    moon->position.x = 3 * cos(2*M_PI*glfwGetTime() * 0.2);
+    moon->position.z = -3 * sin(2*M_PI*glfwGetTime() * 0.2);
     scene.draw_scene();
   }
 }
