3906a5674713c4dbebc765aa8d6480c4cb3fd05f
Terna <tkpamber@yahoo.com>
2018-02-15 23:47:22 -0800

    update rubkis app

--- src/apps/rubiks/rubiks.cpp
+++ src/apps/rubiks/rubiks.cpp
@@ -1,4 +1,48 @@
+#include <graphics/scene.h>
 #include "rubiks.h"
+#include <numeric>
+using namespace std;
+
+RubiksCube::RubiksCube() {
+  vector<uchar> idxs(27,0);
+  std::iota(idxs.begin(), idxs.end(), 0);
+  grid = cv::Mat(3,3,CV_8UC3, idxs.data());
+}
+
+RubiksCube::Plane RubiksCube::rotate_plane(RubiksCube::Plane plane, bool ckw) {
+  cv::Mat face = cv::Mat(3,3,CV_8U, plane.data());
+  cv::rotate(face, face, ckw ? cv::ROTATE_90_CLOCKWISE : cv::ROTATE_90_COUNTERCLOCKWISE);
+  return plane;
+}
+
+void RubiksCube::update_cube(RubiksCube::Plane plane, RubiksCube::Plane new_plane) {
+  cv::Mat grid_clone = grid.clone();
+  for(int i = 0; i < plane.size(); i++) {
+    grid_clone.at<uchar>(plane[i]) = grid.at<uchar>(new_plane[i]);
+  }
+  grid = grid_clone;
+}
 
 int main(int argc, char* args[]) {
+  shared_ptr<graphics::Canvas> canvas = make_shared<graphics::Canvas>();
+  graphics::Scene3D scene(canvas);
+
+  auto cube = RubiksCube();
+  auto plane = cube.plane_F;
+  auto rotated_plane = cube.rotate_plane(plane);
+  cv::Mat face1 = cv::Mat(3,3,CV_8U, plane.data()).clone();
+  cv::Mat face2 = cv::Mat(3,3,CV_8U, rotated_plane.data()).clone();
+  cout << face1 << endl << face2 << endl;
+
+//  std::shared_ptr<graphics::Solid> earth = graphics::SolidShape::make_sphere(2, 10, 10);
+//  earth->material.color = glm::vec3(0.1, 0.1, 0.8);
+//
+//  scene.add_drawable(earth);
+//
+//  while(canvas->still_open()) {
+//    earth->rotation.y += 1;
+//    earth->rotation.x += 1;
+//    scene.draw_scene();
+//  }
 }
+
---
+++ src/apps/rubiks/rubiks.h
@@ -0,0 +1,22 @@
+#include <graphics/shape.h>
+#include <opencv2/opencv.hpp>
+
+struct RubiksCube {
+  typedef std::vector<char> Plane;
+  enum struct Move {
+    L, R, T, Bt, F, Bk, H, V
+  };
+  cv::Mat grid;
+  //front facing views
+  std::vector<uchar> plane_L = {2,1,0, 11,10,9, 20,19,18};
+  std::vector<uchar> plane_R = {6,7,8, 15,16,17, 24,25,26};
+  std::vector<uchar> plane_T = {2,5,8, 1,4,7, 0,3,6};
+  std::vector<uchar> plane_Bt = {18,21,24, 19,22,25, 20,23,26};
+  std::vector<uchar> plane_F = {0,3,6, 9,12,15, 18,21,24};
+  std::vector<uchar> plane_Bk = {8,5,2, 17,14,11, 26,23,20};
+
+//public:
+  RubiksCube();
+  RubiksCube::Plane rotate_plane(Plane plane, bool ckw = true);
+  void update_cube(RubiksCube::Plane plane, RubiksCube::Plane new_plane);
+};
