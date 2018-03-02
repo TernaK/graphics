d81cbbff00e9d44d2e40fa25ca5d174f695a8192
Terna <tkpamber@yahoo.com>
2018-03-01 22:31:59 -0800

    multi threaded ray cast

--- src/apps/ray_scene.cpp
+++ src/apps/ray_scene.cpp
@@ -13,10 +13,7 @@ int main(int argc, char* args[]) {
   shared_ptr<Canvas> canvas = make_shared<Canvas>(400,300,true);
 
   ImplicitNode node = ImplicitNode(ShapeType::sphere);
-//  node.scale *= 2;
-  //make ray
-  ray_t ray;
-  ray.p = glm::vec3(0,0,10);
+  node.scale *= 1.5;
 
   Light light;
   light.position = glm::vec3(10,10,10);
@@ -28,28 +25,37 @@ int main(int argc, char* args[]) {
 //  for(;;) {
     parent_transformable.position.y = 0.4 * sin(glfwGetTime());
     transform_t p_transform = parent_transformable.get_transform();
-  cv::Mat image = cv::Mat::zeros(100,100,CV_32FC3);
-    node.position.x = 0.5 * sin(glfwGetTime());
-    node.position.y = 0.5 * cos(glfwGetTime());
+    cv::Mat image = cv::Mat::zeros(400,400,CV_32FC3);
+    node.position.x = 1 * sin(glfwGetTime());
+    node.position.y = 1 * cos(glfwGetTime());
     node.rotation.z += 3;
     node.rotation.y += 5;
     node.rotation.x += 3;
     transform_t transform = node.get_transform() << p_transform;
     glm::vec3 clear_color(0.1);
 
+    int width = image.cols;
+    int height = image.rows;
 //    for(int i = 0; i < image.rows; i++) {
 //      for(int j = 0; j < image.cols; j++) {
-  image.forEach<cv::Vec3f>([&](cv::Vec3f& frag, const int* loc) {
-//    printf("%d %d\n", loc[1], loc[0]);
-        ray.d = glm::normalize(glm::vec3((float(loc[1]) * 2.0 / image.cols) - 1.0, -((float(loc[0]) * 2.0 / image.rows) - 1), -3));
+    image.forEach<cv::Vec3f>([&](cv::Vec3f& frag, const int* row_col) {
+      //make ray
+        ray_t ray;
+        ray.p = glm::vec3(0,0,10);
+        int row = row_col[0];
+        int col = row_col[1];
+//        auto& frag = image.at<cv::Vec3f>(i,j);
+        float ray_dx = (float(col) * 2.0 / width) - 1.0;
+        float ray_dy = -((float(row) * 2.0 / height) - 1);
+        ray.d = glm::normalize(glm::vec3(ray_dx, ray_dy, -3));
 
         hit_t hit;
         float cos_t = 0;
         Material material;
         glm::vec3 color = clear_color;
         if(node.ray_hit_test(ray, hit, transform, material)) {
-          material.shininess = 2;
-          material.strength.x = 0.4;
+          material.shininess = 128;
+          material.strength.x = 0.3;
           material.color = glm::vec3(1.0, 0, 0);
           glm::vec3 ambient, diffuse, specular;
           ambient = material.strength.x * light.ambient;
@@ -65,10 +71,10 @@ int main(int argc, char* args[]) {
           color = (ambient + diffuse + specular) * material.color;
           color = glm::clamp(color, 0.0f, 1.0f);
         }
-    image.at<cv::Vec3f>(loc[0], loc[1]) = cv::Vec3f(color.b, color.g, color.r);
+        frag = cv::Vec3f(color.b, color.g, color.r);
 //      }
 //    }
-  });
+    });
 
     cv::imshow("ray cast", image);
     cv::waitKey();
