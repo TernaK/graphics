13564cb59a1aa41c4cf1d35e3df630de2a19cfcb
Terna <tkpamber@yahoo.com>
2018-03-07 22:24:48 -0800

    clean out Scene

--- include/graphics/scene.h
+++ include/graphics/scene.h
@@ -8,37 +8,6 @@
 #include <vector>
 
 namespace graphics {
-  // Scene3D
-  //__________________________________________________
-  class Scene3D {
-    std::shared_ptr<Shader> prepare_shader(std::shared_ptr<Drawable> drawable);
-    void setup_light_camera(std::shared_ptr<Shader> shader);
-
-  public:
-    std::shared_ptr<Drawable> root;
-    std::shared_ptr<Canvas> canvas;
-    std::shared_ptr<Light> light = std::make_shared<Light>();
-    std::shared_ptr<Camera> camera = std::make_shared<Camera>();
-    std::vector<std::shared_ptr<Drawable>> drawables;
-    std::shared_ptr<Drawable> background;
-
-    std::shared_ptr<Shader> object3d_shader = Shader::make_object3d_shader();
-    std::shared_ptr<Shader> mesh_shader = Shader::make_mesh_point_shader();
-    std::shared_ptr<Shader> sprite_shader = Shader::make_sprite_shader();
-
-    glm::vec4 clear_color = glm::vec4(0.8,0.8,0.8,1);
-
-    Scene3D(std::shared_ptr<Canvas> canvas);
-
-    void draw_scene();
-
-    void add_drawable(std::shared_ptr<Drawable> solid_node);
-
-    void set_background(std::shared_ptr<Drawable> drawable);
-
-    void clear();
-  };
-
   // Scene
   //__________________________________________________
   struct Scene {
